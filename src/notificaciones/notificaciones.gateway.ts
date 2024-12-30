de226e391d7931936d878102929ebdab49846f24
vCif3r <davidar0601@gmail.com>
2024-12-30 16:31:48 -0500

    notificaciones terminadas creo

--- src/notificaciones/notificaciones.gateway.ts
+++ src/notificaciones/notificaciones.gateway.ts
@@ -5,30 +5,30 @@ import { NotificacionesService } from './notificaciones.service';
 @WebSocketGateway(3001, { cors: { origin: '*' } })
 export class NotificationsGateway {
 
-    constructor(
-        private readonly notificationsService: NotificacionesService,
-    ) { }
+  constructor(
+    private readonly notificationsService: NotificacionesService,
+  ) { }
 
-    @WebSocketServer()
-    server: Server;
+  @WebSocketServer()
+  server: Server;
 
-    private users = new Map<string, number>();
+  private users = new Map<string, number>();
 
-    afterInit(server: Server) {
-        console.log('Initialized');
-    }
+  afterInit(server: Server) {
+    console.log('Initialized');
+  }
 
-    handleDisconnect(client: Socket) {
-        console.log(`Client Disconnected: ${client.id}`);
-        this.users.delete(client.id);
-    }
+  handleDisconnect(client: Socket) {
+    console.log(`Client Disconnected: ${client.id}`);
+    this.users.delete(client.id);
+  }
 
-    handleConnection(client: Socket) {
-        console.log(`Client Connected: ${client.id}`);
-        const userId = client.handshake.query.userId;
+  handleConnection(client: Socket) {
+    console.log(`Client Connected: ${client.id}`);
+    const userId = client.handshake.query.userId;
     if (userId) {
       this.users.set(client.id, parseInt(userId.toString(), 10));
-      
+
       // Obtener las notificaciones del usuario desde la base de datos
       this.notificationsService.getUserNotifications(userId)
         .then(notifications => {
@@ -36,20 +36,34 @@ export class NotificationsGateway {
           client.emit('notifications', notifications);
         });
     }
-    }
+  }
 
-    async sendNotificationToUser(userId: number, message: string) {
-        // Guardar la notificación en la base de datos
-        const notificacion = await this.notificationsService.createNotification(userId, message);
-    
-        // Enviar la notificación al cliente conectado (si está conectado)
-        for (const [clientId, id] of this.users.entries()) {
-          if (id === userId) {
-            const client = this.server.sockets.sockets.get(clientId); // Usar sockets.sockets para obtener el cliente
-            if (client) {
-              client.emit('notification', notificacion);
-            }
-          }
+  async sendNotificationToUser(userId: number, message: string) {
+    // Guardar la notificación en la base de datos
+    const notificacion = await this.notificationsService.createNotification(userId, message);
+
+    // Enviar la notificación al cliente conectado (si está conectado)
+    for (const [clientId, id] of this.users.entries()) {
+      if (id === userId) {
+        const client = this.server.sockets.sockets.get(clientId); // Usar sockets.sockets para obtener el cliente
+        if (client) {
+          client.emit('notification', notificacion);
         }
+      }
     }
+  }
+
+  @SubscribeMessage('markNotificationsAsRead')
+  async handleMarkNotificationsAsRead(client: Socket, userId: any) {
+    const validUserId = parseInt(userId, 10);
+    await this.notificationsService.markAsRead(validUserId);
+    for (const [clientId, id] of this.users.entries()) {
+      if (id === validUserId) {
+        const clientSocket = this.server.sockets.sockets.get(clientId);
+        if (clientSocket) {
+          clientSocket.emit('notificationsRead');
+        }
+      }
+    }
+  }
 }
--- src/notificaciones/notificaciones.service.ts
+++ src/notificaciones/notificaciones.service.ts
@@ -42,11 +42,21 @@ export class NotificacionesService {
       relations: ['user'],
       where: { user: {id: userId} },
       order: { createdAt: 'DESC' },
+      take: 6,
     });
   }
 
   // Método para marcar una notificación como leída
-  async markAsRead(notificationId: number): Promise<void> {
-    await this.notificacionRepository.update(notificationId, { leido: true });
+  async markAsRead(idUser: any) {
+    const notifications = await this.notificacionRepository.find({
+      where: { user: { id: idUser }, leido: false }, 
+      relations: ['user']
+    });
+    await notifications.forEach((notification) =>{
+      notification.leido = true;
+      this.notificacionRepository.save(notification);  // Guardar la notificación actualizada
+    })
+    console.log('notifications actualizadas')
+    return;
   }
 }
