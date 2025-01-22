052dd12ed5d50dc5329095eab73b41870cfbca50
vCif3r <davidar0601@gmail.com>
2025-01-22 12:14:31 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthService } from './auth/auth.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const authService = app.get(AuthService);
+
+  const config = new DocumentBuilder()
+    .setTitle('jns example')
+    .setDescription('JNS API description')
+    .setVersion('1.0')
+    .addTag('jns')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await authService.createSuperAdminUser();
   await authService.createRoles();
